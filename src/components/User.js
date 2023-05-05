890f317914214c280eb9f7e6f3720f85a61f9e64
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-05-04 23:37:37 -0300

    Final - Correções de layout e data-test

--- src/components/User.js
+++ src/components/User.js
@@ -17,19 +17,21 @@ export default function User() {
   function changeUserImage(){
     const image = prompt("Digite o link da sua nova Imagem");
     if(!image){
-      alert("Nome Invalido");
+      alert("Link Invalido");
     }else{
       setImage(image);
     }
   };
   
   return (
-    <div className="usuario">S
+    <div className="usuario">
       <div className="perfil-usuario">
         <img data-test="profile-image" onClick={changeUserImage} src={UserImage} alt={UserName} />
         <div className="info">
-          <a href={dataUser.link}><span data-test="edit-name">{UserName}</span></a>
+          <div>
+          <a data-test="name" href={dataUser.link}><span>{UserName}</span></a>
           <p data-test="name" >{UserName}</p>
+          </div>
           <button data-test="edit-name" onClick={changeUserName}><ion-icon name="pencil-sharp"></ion-icon></button>
         </div>
       </div>
